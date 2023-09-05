484200564ef482c2604f7f30f9853123fc25e502
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-05 17:51:43 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+import "./styles/utilities.css";
+import "./styles/colors.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
