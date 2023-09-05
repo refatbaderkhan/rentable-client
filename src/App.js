137414769c2e5f9f040b52de27d1ca39f84bfe06
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-05 18:44:47 +0300

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./styles/App.css";
 import "./styles/utilities.css";
 import "./styles/colors.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Authentication from "./pages/Authentication";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Authentication />} />
+          <Route path="/landing" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
