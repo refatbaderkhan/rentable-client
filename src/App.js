925e5359a4d13a59843dff08a08d66e39c9f6a47
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 10:12:53 +0300

    updated BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import "./styles/App.css";
 import "./styles/utilities.css";
 import "./styles/colors.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Authentication from "./pages/Authentication";
+import NavBar from "./components/ui/NavBar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
+import CreateItem from "./pages/CreateItem";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<Authentication />} />
-          <Route path="/landing" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/add-item" element={<CreateItem />} />
         </Routes>
       </BrowserRouter>
     </div>
