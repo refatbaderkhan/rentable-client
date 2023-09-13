8b143fa707bbdbef29e27eab26cb80a35adcc762
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 10:04:05 +0300

    added CreateItem page

---
+++ src/pages/CreateItem/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateItemForm from '../../components/ui/CreateItemForm'
+
+const CreateItem = () => {
+  return (
+    <div className="flex center page">
+      <CreateItemForm />
+    </div>
+  );
+};
+
+export default CreateItem
