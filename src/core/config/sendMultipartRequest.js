6f5aee8bbd1729a5649bdc83704d8eb4cfde59e1
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 10:02:19 +0300

    updated sendMultipartRequest config

--- src/core/config/sendMultipartRequest.js
+++ src/core/config/sendMultipartRequest.js
@@ -1,33 +1,50 @@
-import axios from "axios";
-import { localStorageAction } from "./localstorage";
+  import axios from "axios";
+  import { localStorageAction } from "./localstorage";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+  axios.defaults.baseURL = "http://127.0.0.1:8000";
 
-export const sendMultipartRequest = async ({
-  method = "GET",
-  route,
-  body,
-  includeHeaders = true,
-}) => {
-  if (!route) throw Error("URL required");
+  export const sendMultipartRequest = async ({
+    method = "GET",
+    route,
+    body,
+    files,
+    includeHeaders = true,
+  }) => {
+    if (!route) throw Error("URL required");
 
-  const headers = includeHeaders
-    ? {
-        Authorization: `Bearer ${localStorageAction("access_token")}`,
-        "Content-Type": "multipart/form-data",
-      }
-    : {};
+    const headers = includeHeaders
+      ? {
+          Authorization: `Bearer ${localStorageAction("access_token")}`,
+          "Content-Type": "multipart/form-data",
+        }
+      : {};
 
-  try {
-    const response = await axios.request({
-      method,
-      url: route,
-      data: body,
-      headers: headers,
-    });
+    const formData = new FormData();
+    if (files) {
+      files.forEach((file) => {
+        formData.append("files", file);
+      });
+    }
 
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
+    if (body) {
+      Object.keys(body).forEach((key) => {
+        formData.append(key, body[key]);
+      });
+    }
+
+    try {
+      const response = await axios.request({
+        method,
+        url: route,
+        data: formData,
+        headers: {
+          ...headers,
+          "Content-Type": "multipart/form-data", 
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
