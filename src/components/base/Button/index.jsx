23486de1d9ef9e04c5d1d40d75af73e86b65505b
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:56:05 +0300

    added type prop to Button

--- src/components/base/Button/index.jsx
+++ src/components/base/Button/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-const Button = ({ text, color, textColor, onClick, style, enabled = true }) => {
+const Button = ({ text, color, textColor, onClick, style, enabled = true, type }) => {
   const clickHandler = () => {
     if (enabled) {
       onClick();
@@ -10,10 +10,9 @@ const Button = ({ text, color, textColor, onClick, style, enabled = true }) => {
 
   const combinedClassName = `baseButton${style === "Alternative" ? "Alternative" : (style === "Alternative2" ? "Alternative2" : "")}`;
 
-  console.log('2',combinedClassName)
-
   return (
     <button
+      type = {`${type}`}
       className={` ${combinedClassName} pointer ${color} d-text`}
       onClick={() => clickHandler()}
     >
