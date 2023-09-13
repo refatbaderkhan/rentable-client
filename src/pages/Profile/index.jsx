b61906ae052291ca4de17bd13b3ff4c80e876d70
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 10:11:58 +0300

    added Profile Page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Profile = () => {
+
+//const [user, setUser] = useState([])
+//
+//const user_id = '64fcfeca9b5ee85dc26aaedc'
+//
+//const getUser = async () => {
+//  const response = await sendRequest({
+//    route: `user/${user_id}`,
+//  })
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
