97ab32832bdf3506ad854abab1dc229c6716a960
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:06:14 +0300

    added chatSlice function

--- src/redux/chat/chatSlice.js
+++ src/redux/chat/chatSlice.js
@@ -3,3 +3,13 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
   room_id: "",
 }
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState: initialState,
+  reducers: {
+    setChat: (state, action) => {
+      state.room_id = action.payload.room_id
+    }
+  }
+});
