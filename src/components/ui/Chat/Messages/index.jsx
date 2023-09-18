1b485b6123afb4f675d1b89dc64a8179898e2b42
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-18 09:32:07 +0300

    added getMessages function useEffect

--- src/components/ui/Chat/Messages/index.jsx
+++ src/components/ui/Chat/Messages/index.jsx
@@ -1,8 +1,25 @@
 import {useState, useEffect} from 'react'
+import { sendRequest } from '../../../../core/config/request';
+import { requestMethods } from '../../../../core/enums/requestMethods';
 
 const Messages = ({socket, room_id}) => {
   const [messagesRecieved, setMessagesReceived] = useState([]);
   const [messagesHistory, setMessagesHistory] = useState([]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const response = await sendRequest({
+          method: requestMethods.GET,
+          route: `/user/messages/${room_id}`,
+        });
+        setMessagesHistory(response);
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    };
+    getMessages();
+  }, []);
 
 
 export default Messages;
