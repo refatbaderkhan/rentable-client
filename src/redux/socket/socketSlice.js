664d8f42bd1e394d76914f67c34141dadb566c7a
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:04:08 +0300

    added socketSlice function

--- src/redux/socket/socketSlice.js
+++ src/redux/socket/socketSlice.js
@@ -3,3 +3,13 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
   socket: "",
 }
+
+const socketSlice = createSlice({
+  name: "chat",
+  initialState: initialState,
+  reducers: {
+    setSocket: (state, action) => {
+      state.socket = action.payload.socket
+    }
+  }
+});
