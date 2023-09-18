d3a8f9eaa16a48dc9af5d9022df6b0ee738da3ee
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:08:41 +0300

    imported createLogger middleware into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import chatReducer from "./chat/chatSlice";
 import socketReducer from "./socket/socketSlice";
+import { createLogger } from "redux-logger";
 
 const store = configureStore({
   reducer: {
@@ -9,6 +10,7 @@ const store = configureStore({
     chat: chatReducer,
     socket: socketReducer,
   },
+  middleware: [createLogger()],
 });
 
 export default store;
