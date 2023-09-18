2d31da87a5ec20016b525d9fa400a743062c069c
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:01:45 +0300

    added userSlice function

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -16,3 +16,25 @@ const initialState = {
     user_type: "",
   }
 }
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.token = action.payload.token
+      state.user._id = action.payload.user._id
+      state.user.email = action.payload.user.email
+      state.user.username = action.payload.user.username
+      state.user.first_name = action.payload.user.first_name
+      state.user.last_name = action.payload.user.last_name
+      state.user.profile_picture = action.payload.user.profile_picture
+      state.user.user_bookings = action.payload.user.user_bookings
+      state.user.user_favorites = action.payload.user.user_favorites
+      state.user.user_items = action.payload.user.user_items
+      state.user.user_ratings = action.payload.user.user_ratings
+      state.user.user_type = action.payload.user.user_type
+    }
+  }
+});
+
