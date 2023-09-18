41491e1781d1e9cb14a2190050d50993f9c70c2d
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-18 19:51:31 +0300

    added DraggableMarker map function

--- src/components/base/Map/index.jsx
+++ src/components/base/Map/index.jsx
@@ -22,6 +22,39 @@ const Map = ({onCoordinatesChange}) => {
     return null;
   };
 
+
+  function DraggableMarker() {
+    const markerRef = useRef(null)
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          const marker = markerRef.current
+          if (marker != null) {
+            const { lat, lng } = marker.getLatLng()
+            setPosition([lat, lng]);
+            onCoordinatesChange({
+              item_latitude: lat,
+              item_longitude: lng,
+            })
+          }
+        },
+      }),
+      [],
+    )
+
+    
+    return (
+      <Marker
+        draggable={true}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+        autoPan={true}
+        >
+      </Marker>
+    )
+  }
+
 }
 
 export default Map
