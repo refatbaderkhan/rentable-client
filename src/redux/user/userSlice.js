6588dcbfff6ddffd26ad7c693febdc1d924bd3bb
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:01:04 +0300

    added intialState to userSlice

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,2 +1,18 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+const initialState = {
+  token: "",
+  user: {
+    _id: "",
+    email: "",
+    username: "",
+    first_name: "",
+    last_name: "",
+    profile_picture: "",
+    user_bookings: [],
+    user_favorites: [],
+    user_items: [],
+    user_ratings: [],
+    user_type: "",
+  }
+}
