0b72f8c116aab5e4787b977b377e4a07549f5f20
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:07:58 +0300

    added user,chat and socket reducers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import chatReducer from "./chat/chatSlice";
+import socketReducer from "./socket/socketSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+    chat: chatReducer,
+    socket: socketReducer,
+  },
 });
 
 export default store;
