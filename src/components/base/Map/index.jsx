542ea9d04fb9bb9f5e91290033b712d83c676baa
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-18 19:49:05 +0300

    added Map to base components

---
+++ src/components/base/Map/index.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useRef, useMemo, useEffect } from 'react'
+import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet'
+import "./style.css";
+
+const Map = ({onCoordinatesChange}) => {
+
+export default Map
