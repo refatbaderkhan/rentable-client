9891726a4ce8f749d6ad3b793f2ba09c2feaff14
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-18 09:30:39 +0300

    intiallized messagesRecieved and messagesHistory states

--- src/components/ui/Chat/Messages/index.jsx
+++ src/components/ui/Chat/Messages/index.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react'
 
 const Messages = ({socket, room_id}) => {
+  const [messagesRecieved, setMessagesReceived] = useState([]);
+  const [messagesHistory, setMessagesHistory] = useState([]);
 
 
 export default Messages;
