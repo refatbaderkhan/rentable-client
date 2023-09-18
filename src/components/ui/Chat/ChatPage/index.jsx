5c1e47e839d541aaa718272410f8f7b7f7cbb5d1
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-18 09:38:46 +0300

    returned Messages component in ChatPage

--- src/components/ui/Chat/ChatPage/index.jsx
+++ src/components/ui/Chat/ChatPage/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Messages from '../Messages'
 
 const ChatPage = ({user_id, socket, room_id}) => {
 
+  return (
+    <div>
+      <Messages
+        socket = {socket}
+        room_id = {room_id}
+      />
+    </div>
+  )
 }
 
 export default ChatPage
