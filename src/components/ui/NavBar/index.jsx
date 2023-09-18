96902c5c3cfb745840f13138d5cf39a99ac81060
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:12:59 +0300

    add socket and socket dispatch function to NavBar

--- src/components/ui/NavBar/index.jsx
+++ src/components/ui/NavBar/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import './style.css'
+import { setSocket } from '../../../redux/socket/socketSlice';
+import { useDispatch } from "react-redux";
+import io from "socket.io-client";
+
+
+const socket = io.connect("http://127.0.0.1:4000");
 
 const NavBar = () => {
+
+  const dispatch = useDispatch();
+  dispatch(setSocket({socket: socket}));
+
   return (
     <div className="navbar">
       NavBar
