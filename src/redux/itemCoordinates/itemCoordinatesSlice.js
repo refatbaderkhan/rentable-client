f72ab2dd21394aa91e3767fcdfaf20e217f2a016
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:23:20 +0300

    added itemCoordinatesSlice function

--- src/redux/itemCoordinates/itemCoordinatesSlice.js
+++ src/redux/itemCoordinates/itemCoordinatesSlice.js
@@ -4,3 +4,15 @@ const initialState = {
   item_latitude: "",
   item_longitude: "",
 }
+
+const itemCoordinatesSlice = createSlice({
+  name: "coordinates",
+  initialState: initialState,
+  reducers: {
+    setCoordinates: (state, action) => {
+      state.item_latitude = action.payload.item_latitude
+      state.item_longitude = action.payload.item_longitude
+    }
+  }
+});
+
