21fc81f37631a0b323010dc220a1b166eb9797c9
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 01:47:21 +0300

    imported coordinates reducer into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import chatReducer from "./chat/chatSlice";
 import socketReducer from "./socket/socketSlice";
+import coordinatesReducer from "./itemCoordinates/itemCoordinatesSlice";
 import { createLogger } from "redux-logger";
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
     user: userReducer,
     chat: chatReducer,
     socket: socketReducer,
+    coordinates: coordinatesReducer,
   },
   middleware: [createLogger()],
 });
