bc961b1a8689e177b4c58b9101ef6479fe297aa9
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 12:43:16 +0300

    added initial CreateCategory form

---
+++ src/components/ui/CreateCategory/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css";
+import Button from '../../base/Button';
+import Input from '../../base/Input';
+import { sendRequest } from '../../../core/config/request';
+import { requestMethods } from '../../../core/enums/requestMethods';
+
+
+const CreateCategory = () => {
+  return (
+    <div>CreateCategory</div>
+  )
+}
+
+export default CreateCategory
