724a3d336a99ea6fc332f962bd41c2d79cab4436
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-20 00:25:18 +0300

    updated AdminCategory component

--- src/components/base/AdminCategory/index.jsx
+++ src/components/base/AdminCategory/index.jsx
@@ -31,54 +31,24 @@ const AddSubCategory = () => {
 }
 
 
-const AdminCategory = () => {
-
-   const categoryObj = {
-    "_id": "64fdc3641f1098002b1ca27f",
-    "category_name": "Electronics",
-    "subCategories_names": [
-        "TVs",
-        "khara",
-        "zeft",
-        "shkhakh"
-    ],
-    "subCategorySchema": [
-        {
-            "subCategory_name": "TVs",
-            "category_id": "64fdc3641f1098002b1ca27f",
-            "_id": "64fdc39b1f1098002b1ca283"
-        },
-        {
-            "subCategory_name": "Mobiles",
-            "category_id": "64fdc3641f1098002b1ca27f",
-            "_id": "6509c5ac03a0d28bd07a2f7b"
-        },
-        {
-            "subCategory_name": "Washing Machines",
-            "category_id": "64fdc3641f1098002b1ca27f",
-            "_id": "6509c5b403a0d28bd07a2f82"
-        },
-        {
-            "subCategory_name": "ACs",
-            "category_id": "64fdc3641f1098002b1ca27f",
-            "_id": "6509c5bb03a0d28bd07a2f8a"
-        }
-    ],
-    "__v": 4
-  }
+const AdminCategory = ({category}) => {
 
   return (
-    <div className="form-container">
+    <div key={category._id} className="form-container">
       <div className="log-in">
       <div className="spacer-30"></div>
-      <h1>{categoryObj.category_name}</h1>
+      <h1>{category.category_name}</h1>
       <div className="spacer-30"></div>
       <h3>
-      {categoryObj.subCategorySchema.map(SubCategory)}
+          {category.subCategorySchema.length > 0 ? (
+            category.subCategorySchema.map(SubCategory)
+          ) : (
+            <p>No subcategories available</p>
+          )}
       </h3>
       <AddSubCategory />
       <div className="spacer-20"></div>
-      <button onClick={() => console.log(categoryObj._id)}>
+      <button onClick={() => console.log(category._id)}>
         delete category
       </button>
       <div className="spacer-10"></div>
