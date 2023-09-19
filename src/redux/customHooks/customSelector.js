3555720e62aa946a8567823d3ff9f7868d9d11b9
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 11:27:12 +0300

    exported custom selectors

--- src/redux/customHooks/customSelector.js
+++ src/redux/customHooks/customSelector.js
@@ -1,1 +1,14 @@
 import { useSelector } from "react-redux";
+
+export const useCustomSelector = () => {
+  
+    const user = useSelector((state) => state.user);
+  
+    const socket = useSelector((state) => state.socket);
+  
+    const coordinates = useSelector((state) => state.coordinates);
+  
+    const chat = useSelector((state) => state.chat);
+
+  return { user, socket, coordinates, chat }
+}
