42c8e0ecb0aca78319825e1bb2540cc7bbb7ffff
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 14:23:34 +0300

    added initial state and reducers to categorySlice

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -1,2 +1,21 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+
+const initialState = {
+  categories: [],
+}
+
+
+const categorySlice = createSlice({
+  name: "category",
+  initialState: initialState,
+  reducers: {
+    setCategories: (state, action) => {
+      state.categories = action.payload.categories
+    }
+  },
+});
+
+export const {setCategories} = categorySlice.actions
+
+export default categorySlice.reducer
