8ddedcf2ba8bfb2703826911e8aba774484d5432
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-20 02:00:57 +0300

    added addSubCategory reducer to categorySlice

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -15,6 +15,11 @@ const categorySlice = createSlice({
     addCategory: (state, action) => {
       state.categories.push(action.payload.category)
     },
+    addSubCategory: (state, action) => {
+      const {subCategory, category_id} = action.payload
+      const category = state.categories.find(category => category._id === category_id)
+      category.subCategorySchema.push(subCategory)
+    },
     deleteSubCategory: (state, action) => {
       const {subCategory_id, category_id} = action.payload
       const category = state.categories.find(category => category._id === category_id)
