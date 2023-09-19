b831a02e181cd2907bf1b0063d1132e6ca835c2a
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-20 01:29:17 +0300

    added deleteSubCategory reducer to categorySlice

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -1,6 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-
 const initialState = {
   categories: [],
 }
@@ -15,6 +14,12 @@ const categorySlice = createSlice({
     },
     addCategory: (state, action) => {
       state.categories.push(action.payload.category)
+    },
+    deleteSubCategory: (state, action) => {
+      const {subCategory_id, category_id} = action.payload
+      const category = state.categories.find(category => category._id === category_id)
+      const subCategoryIndex = category.subCategorySchema.findIndex(subCategory => subCategory._id === subCategory_id)
+      category.subCategorySchema.splice(subCategoryIndex, 1)
     }
   },
 });
