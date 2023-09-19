8dc3b3ee3b8fc608607a7d5ad11c4433373460bb
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 11:28:33 +0300

    exported custom dispatches

--- src/redux/customHooks/customDispatch.js
+++ src/redux/customHooks/customDispatch.js
@@ -1,1 +1,16 @@
 import { useDispatch } from "react-redux";
+
+export const useCustomDispatch = () => {
+
+  const dispatch = useDispatch();
+  
+  const setUser = (user) => dispatch({ type: "user/setUser", payload: user });
+
+  const setSocket = (socket) => dispatch({ type: "socket/setSocket", payload: socket });
+
+  const setCoordinates = (coordinates) => dispatch({ type: "coordinates/setCoordinates", payload: coordinates });
+
+  const setChat = (chat) => dispatch({ type: "chat/setChat", payload: chat });
+
+return { setUser, setSocket, setCoordinates, setChat }
+}
