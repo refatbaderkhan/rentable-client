3e7ae00ee7a8f5aee36b525b8ee11f49b3f4a2f1
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-20 00:44:08 +0300

    imported AdminCreateCategory and AdminDisplayCategories components into AdminCategoryManagement

--- src/pages/Admin/AdminCategoryManagement/AdminCategoryManagement.jsx
+++ src/pages/Admin/AdminCategoryManagement/AdminCategoryManagement.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./style.css";
+import AdminCreateCategory from '../../../components/ui/AdminCategoryManagement/AdminCreateCategory';
+import AdminDisplayCategories from '../../../components/ui/AdminCategoryManagement/AdminDisplayCategories';
 
 const AdminCategoryManagement = () => {
   return (
-    <div>AdminCategoryManagement</div>
+    <div>
+      <AdminCreateCategory />
+      <AdminDisplayCategories />
+    </div>
   )
 }
 
