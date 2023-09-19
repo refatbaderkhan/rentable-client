b370f7eaf61a3414b2ddd3a9da36bb4d971adff9
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-19 14:26:49 +0300

    updated imports

--- src/components/ui/CategoryManagement/CreateCategory/index.jsx
+++ src/components/ui/CategoryManagement/CreateCategory/index.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 import "./style.css";
-import Button from '../../base/Button';
-import Input from '../../base/Input';
-import { sendRequest } from '../../../core/config/request';
-import { requestMethods } from '../../../core/enums/requestMethods';
+import Button from '../../../base/Button';
+import Input from '../../../base/Input';
+import { sendRequest } from '../../../../core/config/request';
+import { requestMethods } from '../../../../core/enums/requestMethods';
 
 
 const CreateCategory = () => {
