e9f186c2d1a1c36c1a5e20092555ee0880addfca
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-20 11:36:30 +0300

    switch useSocket to useCustomDispatch

--- src/components/ui/NavBar/index.jsx
+++ src/components/ui/NavBar/index.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react'
 import './style.css'
 import {sendRequest} from '../../../core/config/request'
 import {requestMethods} from '../../../core/enums/requestMethods'
-import { setSocket } from '../../../redux/socket/socketSlice';
-import { useDispatch } from "react-redux";
 import io from "socket.io-client";
-import { useEffect } from 'react'
 import { useCustomDispatch } from '../../../redux/customHooks/customDispatch';
 
 
 const socket = io.connect("http://127.0.0.1:4000");
 
 const NavBar = () => {
-  const dispatch = useDispatch();
-  dispatch(setSocket({socket: socket}));
 
-  const {setCategories} = useCustomDispatch();
+  const {setCategories, setSocket} = useCustomDispatch();
+  setSocket({socket: socket});
 
   const category = async () => {
     try {
