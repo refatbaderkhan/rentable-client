b2d7c00d06fc693517dd4123ec57b8d09e948ef6
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 22:08:30 +0300

    added authenticatedRoute function to index

--- src/App.js
+++ src/App.js
@@ -13,12 +13,22 @@ import Chat from "./pages/Chat";
 import ChatPage from "./components/ui/Chat/ChatPage";
 import AdminCategoryManagement from "./pages/Admin/AdminCategoryManagement/AdminCategoryManagement";
 import AdminLocationManagement from "./pages/Admin/AdminUserManagement/AdminLocationManagement";
+import AdminDisplayUsers from "./components/ui/Admin/AdminUserManagement/AdminDisplayUsers";
 import { Provider } from "react-redux";
 import reduxStore from "./redux/store";
+import { Navigate } from "react-router-dom";
 
 
 function App() {
-
+  const [user_type, setUserType] = useState(1);
+  const authenticatedRoute = (Component) => {
+    if (user_type == 0) {
+      return <Component />
+    } else {
+      return <Navigate to="/login" />
+    }
+    
+  };
   return (
     <Provider store={reduxStore}>
     <div className="App">
@@ -32,6 +42,7 @@ function App() {
           <Route path="/add-item" element={<CreateItem />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/chat-page" element={<ChatPage />} />
+          <Route path="/users-management" element={authenticatedRoute(AdminDisplayUsers)} />
           <Route path="/category-management" element={<AdminCategoryManagement />} />
           <Route path="/location-management" element={<AdminLocationManagement />} />
           <Route path="*" element={<h1>Not Found</h1>} />
