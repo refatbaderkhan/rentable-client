48b907eb6be7b2edda98c3efa3a5cc7d552a9a62
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 22:02:39 +0300

    added deleteFromUsers reducer to usersSlice

--- src/redux/slices/users/usersSlice.js
+++ src/redux/slices/users/usersSlice.js
@@ -10,10 +10,15 @@ const usersSlice = createSlice({
   reducers: {
     setUsers: (state, action) => {
       state.users = action.payload.users
-  }
+    },
+    deleteFromUsers: (state, action) => {
+      const user_id = action.payload
+      const userIndex = state.users.findIndex(user => user._id === user_id)
+      state.users.splice(userIndex, 1)
+    }
 }
 });
 
-export const {setUsers} = usersSlice.actions
+export const {setUsers, deleteFromUsers} = usersSlice.actions
 
 export default usersSlice.reducer
