9b74f96b052ed3a63d6d00f9cde381479e78a1ce
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 12:22:51 +0300

    imported usersReducer into redux configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from "./slices/user/userSlice";
 import chatReducer from "./slices/chat/chatSlice";
 import socketReducer from "./slices/socket/socketSlice";
 import coordinatesReducer from "./slices/itemCoordinates/itemCoordinatesSlice";
+import usersReducer from "./slices/users/usersSlice";
 import categoryReducer  from "./slices/category/categorySlice";
 import cityReducer from "./slices/city/citySlice";
 import { createLogger } from "redux-logger";
@@ -13,6 +14,7 @@ const store = configureStore({
     chat: chatReducer,
     socket: socketReducer,
     coordinates: coordinatesReducer,
+    users: usersReducer,
     category: categoryReducer,
     city: cityReducer,
   },
