41dd12a025e7ff760526d643794c25b4bd1c1555
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 04:25:05 +0300

    added styles to Input component

--- src/components/base/Input/index.jsx
+++ src/components/base/Input/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
 
-const Input = ({ onChange, label, placeholder, type = "text" }) => {
+const Input = ({ onChange, label, placeholder, type = "text", width  }) => {
+
+  width = width ? `width-${width}` : "width-400";
+
   return (
     <div className="flex column baseInput">
       <label>{label}</label>
       <input
-        className="roundedMedium"
+        className= {width}
         type={type}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
