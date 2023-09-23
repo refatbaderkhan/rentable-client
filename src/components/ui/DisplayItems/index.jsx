f8cd2fbfbb5640ea23233bdeb84ffb3d7a398224
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-23 12:56:49 +0300

    added search selector and added searchTerm to filter searchItems

--- src/components/ui/DisplayItems/index.jsx
+++ src/components/ui/DisplayItems/index.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import { useCustomSelector } from '../../../redux/customHooks/customSelector'
 import ItemCard from '../../base/ItemCard'
+import Input from '../../base/Input'
+import Button from '../../base/Button'
+
 
 const DisplayItems = () => {
 
-  const {items} = useCustomSelector()
+  const { items, search} = useCustomSelector();
+
+  const [searchTerm, setSearchTerm] = useState(search);
+
+  const searchedItems = items.filter((item) =>
+    item.item_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const [filterToggle, setFilterToggle] = useState(false);
+
+  useEffect(() => {
+    setSearchTerm(search);
+  }, [search])
   
   return (
-    items.map((item) => (
+    <div>
+      <Button
+        text="Filter"
+        style={"Alternative"}
+        onClick={() => setFilterToggle((prevFilterToggle) => !prevFilterToggle)}
+        />
+    <div className='flex'>
+    {searchedItems.map((item) => (
       <ItemCard item={item} />
-    ))
+    ))}
+    </div>
+    {filterToggle && (
+      <div className='filter-modal'>
+      </div>
+    )}
+    </div>
   )
 }
 
