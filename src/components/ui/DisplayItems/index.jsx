a6af62108ed46e4e1ee6bd573a34cd7cc809eb7e
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-23 20:07:57 +0300

    added FilterItems and its functions to DisplayItems

--- src/components/ui/DisplayItems/index.jsx
+++ src/components/ui/DisplayItems/index.jsx
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react'
 import './style.css'
 import { useCustomSelector } from '../../../redux/customHooks/customSelector'
 import ItemCard from '../../base/ItemCard'
-import Input from '../../base/Input'
 import Button from '../../base/Button'
+import FilterItems from '../FilterItems'
 
 
 const DisplayItems = () => {
@@ -16,6 +16,29 @@ const DisplayItems = () => {
     item.item_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const [filters, setFilters] = useState({
+    item_category_name: null,
+    item_subcategory_name: null,
+    city_name: null,
+    area: null,
+  });
+
+
+  const filteredItem = filters.item_category_name || filters.item_subcategory_name || filters.city_name || filters.area
+  ? searchedItems.filter((item) => {
+      for (const key in filters) {
+        if (filters[key] !== null) {
+          if (key === 'city_name' || key === 'area') {
+            if (item.item_location[key] !== filters[key]) return false;
+          } else {
+            if (item[key] !== filters[key]) return false;
+          }
+        }
+      }
+      return true;
+    })
+  : searchedItems; 
+
   const [filterToggle, setFilterToggle] = useState(false);
 
   useEffect(() => {
@@ -23,21 +46,24 @@ const DisplayItems = () => {
   }, [search])
   
   return (
-    <div>
-      <Button
-        text="Filter"
-        style={"Alternative"}
-        onClick={() => setFilterToggle((prevFilterToggle) => !prevFilterToggle)}
-        />
-    <div className='flex'>
-    {searchedItems.map((item) => (
-      <ItemCard item={item} />
-    ))}
-    </div>
-    {filterToggle && (
-      <div className='filter-modal'>
+    <div className='items-page'>
+      {filterToggle && (
+        <FilterItems filters={filters} setFilters={setFilters} setFilterToggle={setFilterToggle}/>
+      )}
+      <div className='display-items'>
+        <div>
+        <Button
+          text="Filter"
+          style={"Alternative"}
+          onClick={() => setFilterToggle((prevFilterToggle) => !prevFilterToggle)}
+          />
+        </div>
+        <div className='flex'>
+          {filteredItem.map((item) => (
+            <ItemCard item={item} />
+          ))}
+        </div>
       </div>
-    )}
     </div>
   )
 }
