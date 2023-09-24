12991fdbd2be2f615e731368bfd41a47d94f7391
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-25 01:44:58 +0300

    added route to Item page component

--- src/core/routes/index.jsx
+++ src/core/routes/index.jsx
@@ -9,6 +9,7 @@ import CreateItem from "../../pages/CreateItem";
 import Chat from "../../pages/Chat";
 import ChatPage from "../../components/ui/Chat/ChatPage";
 import DisplayItems from '../../components/ui/DisplayItems';
+import Item from "../../pages/Item";
 import AdminCategoryManagement from "../../pages/Admin/AdminCategoryManagement/AdminCategoryManagement";
 import AdminLocationManagement from "../../pages/Admin/AdminUserManagement/AdminLocationManagement";
 import AdminDisplayUsers from "../../components/ui/Admin/AdminUserManagement/AdminDisplayUsers";
@@ -43,6 +44,7 @@ const AppRoutes = () => {
           <Route path="/chat" element={<Chat />} />
           <Route path="/chat-page" element={<ChatPage />} />
           <Route path="/items" element={<DisplayItems />} />
+          <Route path="/item" element={<Item />} />
           <Route path="/users-management" element={authenticatedRoute(AdminDisplayUsers)} />
           <Route path="/category-management" element={<AdminCategoryManagement />} />
           <Route path="/location-management" element={<AdminLocationManagement />} />
