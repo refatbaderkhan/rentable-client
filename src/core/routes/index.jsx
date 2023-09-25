8b5275fa23d1f2859aede0116fb1f32ea246d052
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-25 03:35:14 +0300

    added route to Profile component

--- src/core/routes/index.jsx
+++ src/core/routes/index.jsx
@@ -4,12 +4,12 @@ import NavBar from '../../components/ui/NavBar';
 import Login from '../../pages/Authentication/Login';
 import Register from "../../pages/Authentication/Register";
 import Landing from "../../pages/Landing";
-import Profile from "../../pages/Profile";
 import CreateItem from "../../pages/CreateItem";
 import Chat from "../../pages/Chat";
 import ChatPage from "../../components/ui/Chat/ChatPage";
 import DisplayItems from '../../components/ui/DisplayItems';
 import Item from "../../pages/Item";
+import Profile from '../../pages/Profile';
 import AdminCategoryManagement from "../../pages/Admin/AdminCategoryManagement/AdminCategoryManagement";
 import AdminLocationManagement from "../../pages/Admin/AdminUserManagement/AdminLocationManagement";
 import AdminDisplayUsers from "../../components/ui/Admin/AdminUserManagement/AdminDisplayUsers";
@@ -39,12 +39,12 @@ const AppRoutes = () => {
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile/:id" element={<Profile />} />
           <Route path="/add-item" element={<CreateItem />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/chat-page" element={<ChatPage />} />
           <Route path="/items" element={<DisplayItems />} />
           <Route path="/item" element={<Item />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/users-management" element={authenticatedRoute(AdminDisplayUsers)} />
           <Route path="/category-management" element={<AdminCategoryManagement />} />
           <Route path="/location-management" element={<AdminLocationManagement />} />
