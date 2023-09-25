62159ccf44eeb48b690469d6df1eb5a57d13f7a8
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-25 17:41:41 +0300

    added ChatCard component to ui

---
+++ src/components/ui/ChatCard/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+const ChatCard = () => {
+  return (
+    <div className='chat-card-container'>
+      <div className='chat-card'>
+        <div className='chat-card-avatar'>
+          <div className='chat-card-letter'>
+            A
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatCard
