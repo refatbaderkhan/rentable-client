d6e426c07bac3e84db713baa3eb331cb137a9bcc
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-26 17:05:47 +0300

    added ChatCard component to ui

--- src/components/ui/ChatCard/index.jsx
+++ src/components/ui/ChatCard/index.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './style.css'
+import { useCustomSelector } from '../../../redux/customHooks/customSelector'
+import { generateImageUrl } from '../../../core/config/generateImageUrl'
 
-const ChatCard = () => {
+const ChatCard = ({chat, key}) => {
+
+  const { user_id } = useCustomSelector()
+  const user = chat.chat_users.find(user => user.id !== user_id)
+  const last = chat.messages[chat.messages.length - 1]
+  const lastMessage = last.message
+
   return (
     <div className='chat-card-container'>
       <div className='chat-card'>
         <div className='chat-card-avatar'>
-          <div className='chat-card-letter'>
-            A
+          { user.profile_picture ?
+            <img src={generateImageUrl(user.profile_picture)} alt='chat-card-picture' className='chat-card-picture' />
+            :
+            <div className='chat-card-letter'>
+              {user.username[0]}
+            </div>
+          }
+        </div>
+        <div className='chat-card-text'>
+          <div className='chat-card-username'>
+            {user.username}
+          </div>
+          <div className='chat-card-last-message'>
+            {lastMessage}
           </div>
         </div>
       </div>
