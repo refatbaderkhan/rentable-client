eab5bac0d6d4c2c1fc35f18f9d862540a689e22f
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-26 17:04:33 +0300

    added Chat component to pages

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,83 +1,63 @@
-import React, { useState }  from 'react'
+import React, { useEffect, useState }  from 'react'
 import './style.css'
-import {useNavigate} from 'react-router-dom'
 import { sendRequest } from '../../core/config/request';
 import { requestMethods } from '../../core/enums/requestMethods';
-import { localStorageAction } from '../../core/config/localstorage';
 import { useCustomDispatch } from '../../redux/customHooks/customDispatch';
 import { useCustomSelector } from '../../redux/customHooks/customSelector';
 import Input from "../../components/base/Input"
 import ChatCard from '../../components/ui/ChatCard';
+import Button from '../../components/base/Button';
+import { useParams } from 'react-router-dom';
+import { generateImageUrl } from '../../core/config/generateImageUrl';
+import ChatMenu from '../../components/ui/ChatMenu';
+import ChatMessages from '../../components/ui/ChatMessages';
+import ChatSend from '../../components/ui/ChatSend';
+
 
 const Chat = () => {
-
-  const navigate = useNavigate();
-  const [reciever, setReciever] = useState('');
+  
+  const {id} = useParams();
+  const [room_id, setRoomId] = useState('');
   const {user_id , socket} = useCustomSelector();
-  const {setChat} = useCustomDispatch();
 
   const joinHandler = async () => {
-    console.log('user_id_chat', user_id)
 
     try {
       const response = await sendRequest({
         method: requestMethods.GET,
-        route: `/user/chat/${reciever}`,
+        route: `/user/chat/${id}`,
       });
 
-      const room_id = response._id;
-      setChat({
-        room_id: response._id
-      });
+      setRoomId(response._id);
       
       socket.emit('join_chat', {user_id, room_id});
-      navigate('/chat-page', { replace: true });
     } catch (error) {
       console.log(error.response.data.message);
     }
   }
 
+  useEffect(() => {
+    joinHandler();
+  }, []);
+
   return (
     <div className="chat-page-container">
-      <div className='chat-menu'>
-        <div className='chat-menu-search'>
-          <div className='chat-menu-search-input'>
-          <Input
-          width={'300'}
-          onChange={(value) => console.log(value)}
-          />
+      <ChatMenu/>
+      <div className='chat-messaging'>
+        <div className='chat-messaging-header'>
+          <div className='chat-messaging-name'>
+            Refat Baderkhan
           </div>
         </div>
-        <div className='chat-menu-list'>
-          <ChatCard/>
+        <div className='chat-messaging-messages'>
+          <ChatMessages room_id={room_id}/>
         </div>
-        <div className='chat-menu-button'>
+        <div>
+        <ChatSend room_id={room_id}/>
         </div>
       </div>
     </div>
   )
-
-
-//return (
-//  <div className="">
-//    <div className="">
-//      <h1>{`<>DevRooms</>`}</h1>
-//      
-//
-//      <input
-//        className=""
-//        onChange={(e) => setReciever(e.target.value)}
-//      />
-//
-//      <button
-//        className=""
-//        onClick={joinHandler}
-//      >
-//        Join Room
-//      </button>
-//    </div>
-//  </div>
-//);
 };
 
 export default Chat
