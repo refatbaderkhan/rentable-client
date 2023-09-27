b57ab213b9b26e69580c86af4d617598fb64d231
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 01:13:49 +0300

    added addUserReview reducer to usersSlice

--- src/redux/slices/users/usersSlice.js
+++ src/redux/slices/users/usersSlice.js
@@ -15,10 +15,15 @@ const usersSlice = createSlice({
       const user_id = action.payload
       const userIndex = state.users.findIndex(user => user._id === user_id)
       state.users.splice(userIndex, 1)
+    },
+    addUserReview: (state, action) => {
+      const {user_id, review} = action.payload
+      const user = state.users.find(user => user._id === user_id)
+      user.user_ratings.push(review)
     }
-}
+  }
 });
 
-export const {setUsers, deleteFromUsers} = usersSlice.actions
+export const {setUsers, deleteFromUsers, addUserReview} = usersSlice.actions
 
 export default usersSlice.reducer
