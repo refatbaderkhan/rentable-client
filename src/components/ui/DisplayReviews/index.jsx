e959ebbc247e50ab58858e092a4815172c003fc5
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 01:17:20 +0300

    added type prop to DisplayReview component

--- src/components/ui/DisplayReviews/index.jsx
+++ src/components/ui/DisplayReviews/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReviewCard from '../ReviewCard'
 
-const DisplayReviews = ({reviews}) => {
+const DisplayReviews = ({reviews, type}) => {
+  
   return (
     <div className='display-reviews'>
       {reviews.map(review => (
-        <ReviewCard key={review._id} review={review} />
+        <ReviewCard key={review._id} review={review} type={type} />
       ))}
     </div>
   )
