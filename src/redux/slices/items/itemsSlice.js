b2730ebe361949125bc79ed58e0b98f97cc6dc06
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-27 23:58:56 +0300

    added addItemReview reducer to itemsSlice

--- src/redux/slices/items/itemsSlice.js
+++ src/redux/slices/items/itemsSlice.js
@@ -11,9 +11,14 @@ const itemsSlice = createSlice({
     setItems: (state, action) => {
       state.items = action.payload.items
     },
+  addItemReview: (state, action) => {
+    const {item_id, review} = action.payload
+    const item = state.items.find(item => item._id === item_id)
+    item.item_ratings.push(review)
+  },
 }
 });
 
-export const {setItems} = itemsSlice.actions
+export const {setItems, addItemReview} = itemsSlice.actions
 
 export default itemsSlice.reducer
