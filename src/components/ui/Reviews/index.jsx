e298a8a988020659e21a4f6eb33b426d6f31eb67
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 01:15:23 +0300

    added type prop to Reviews component

--- src/components/ui/Reviews/index.jsx
+++ src/components/ui/Reviews/index.jsx
@@ -3,15 +3,18 @@ import  './style.css'
 import AddReview from '../AddReview'
 import DisplayReviews from '../DisplayReviews'
 
-const Reviews = ({id, reviews}) => {
+const Reviews = ({id, reviews, type}) => {
+
 
   return (
     <div className='reviews-container'>
       <div>
-        <AddReview id={id} />
+        <AddReview id={id} type={type} />
       </div>
       <div className='reviews-display-reviews'>
-        <DisplayReviews id={id} reviews={reviews}/>
+        {reviews !== 0 && (
+        <DisplayReviews id={id} reviews={reviews} type={type}/>
+        )}
       </div>
     </div>
   )
