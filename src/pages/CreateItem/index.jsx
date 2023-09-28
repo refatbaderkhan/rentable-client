79be8de75ddbb52234ad81f5b66c3998e4fdb9c1
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 20:41:54 +0300

    updated CreateItem page style

--- src/pages/CreateItem/index.jsx
+++ src/pages/CreateItem/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import CreateItemForm from '../../components/ui/CreateItemForm'
+import background from "../../assets/background.png";
 
 const CreateItem = () => {
   return (
-    <div className="flex center page">
+    <div className="flex center ">
+      <div className="wallpaper" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "calc(100vh - 75px)", zIndex: -1}}>
+        <img src={background} alt="Wallpaper" style={{width: "100%", height: "100%", objectFit: "cover"}} />
+      </div>
       <CreateItemForm />
     </div>
   );
