4338e9bec34f8c605d01528076d1583cc50b3711
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 17:33:04 +0300

    updated Register page style

--- src/pages/Authentication/Register/index.jsx
+++ src/pages/Authentication/Register/index.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import "./style.css";
 import RegisterForm from "../../../components/ui/AuthenticationForms/RegisterForm";
+import background from "../../../assets/background.png";
 
 const Register = () => {
 
   return (
     <div className="register-page">
+      <div className="wallpaper" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "calc(100vh - 75px)", zIndex: -1}}>
+        <img src={background} alt="Wallpaper" style={{width: "100%", height: "100%", objectFit: "cover"}} />
+      </div>
       <div className="register-page-container">
-        <div className="register-page-header">
-          You're one step away!
-        </div>
         <RegisterForm />
       </div>
     </div>
   );
 };
+
 
 export default Register;
