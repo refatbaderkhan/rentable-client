7aa9d202fd6c666cefa50eb7653e60ba71d72439
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 17:40:59 +0300

    updated Login page styling

--- src/pages/Authentication/Login/index.jsx
+++ src/pages/Authentication/Login/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./style.css";
 import LoginForm from "../../../components/ui/AuthenticationForms/LoginForm";
+import background from "../../../assets/background.png";
 
 const Login = () => {
 
   return (
     <div className="login-page">
+      <div className="wallpaper" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "calc(100vh - 75px)", zIndex: -1}}>
+        <img src={background} alt="Wallpaper" style={{width: "100%", height: "100%", objectFit: "cover"}} />
+      </div>
       <div className="login-page-container">
         <div className="login-page-header">
           Weclome Back!
