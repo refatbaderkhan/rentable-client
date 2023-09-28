34db7ea5da6965bcc91aae28566f1b6c3e6d9932
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-28 17:30:07 +0300

    updated Dropdown component subDropdown placeholder handling

--- src/components/base/Dropdown/index.jsx
+++ src/components/base/Dropdown/index.jsx
@@ -7,11 +7,18 @@ const Dropdown = ({ onChange, placeHolder, options, type }) => {
   
   const [showMenu, setShowMenu] = useState(false);
   const [selected, setSelected] = useState(null);
+  const [subDisplay, setSubDisplay] = useState(null);
   const [option, setOption] = useState([]);
   
   const getDisplay = () => {
     if (selected) {
       return selected;
+    }
+    if (subDisplay === "Enter your area") {
+      return placeHolder;
+    }
+    if (type === "city") {
+      setSubDisplay("Enter your area");
     }
     return placeHolder;
   };
@@ -35,14 +42,18 @@ const Dropdown = ({ onChange, placeHolder, options, type }) => {
   
 
   return (
-    <div className="flex">
+    <div className="dropdown">
+    <div className="dropdown-label">
+      <label>{placeHolder}</label>
+    </div>
+    <div className="double-dropdown">
     <div className="dropdown-container">
       <div onClick={()=> setShowMenu((prev)=> !prev)}
       className="dropdown-input">
         <div className="dropdown-selected-value">{getDisplay()}</div>
         <div className="dropdown-tools">
           <div className="dropdown-tool">
-          <svg height="20" width="20" viewBox="0 0 20 20">
+          <svg height="" width="20" viewBox="0 0 20 20" >
             <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
           </svg>
           </div>
@@ -63,7 +74,9 @@ const Dropdown = ({ onChange, placeHolder, options, type }) => {
       </div>
       }
     </div>
-    <SubDropdown onChange={onChange} placeHolder={placeHolder} main={selected} options={option} />
+    <div className="drop-down-seperator"></div>
+    <SubDropdown onChange={onChange} placeHolder={subDisplay} main={selected} options={option} />
+    </div>
     </div>
   );
 };
