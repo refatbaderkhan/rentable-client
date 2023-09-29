6e009233bfbfbc049460f3e98cefef48ba6f2235
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-29 14:34:58 +0300

    updated AdminDashboardEntry style

--- src/components/ui/AdminDashboardEntry/index.jsx
+++ src/components/ui/AdminDashboardEntry/index.jsx
@@ -89,9 +89,6 @@ const AdminDashboardEntry = ({behavior, map}) => {
   return (
     <div className='admin-dashboard-content-table-entry'>
       <div className='admin-dashboard-content-table-entry-main'>
-        <div className='admin-dashboard-content-table-entry-counter'>
-          1
-        </div>
         <div className='admin-dashboard-content-table-entry-name'>
           {entryName}
         </div>
@@ -102,33 +99,33 @@ const AdminDashboardEntry = ({behavior, map}) => {
             onClick={() => handleMainDelete()}
           />
         </div>
-        {ChangePassword && (
-        <div className='admin-dashboard-content-table-entry-password'>
-          <Button
-            text='Change Password'
-            style={'Alternative'}
-            onClick={() => console.log('change password')}
-          />
-        </div>
-        )}
+        {
+        //ChangePassword && (
+        //<div className='admin-dashboard-content-table-entry-password'>
+        //  <Button
+        //    text='Change Password'
+        //    style={'Alternative'}
+        //    onClick={() => console.log('change password')}
+        //  />
+        //</div>
+        //)
+        }
         {notUser && (
         <div className='flex'>
         <div className='admin-dashboard-content-table-entry-subcreate'>
           <Button
-            text='Create Sub Entry'
-            style={'Alternative'}
+            text='Create Sub'
+            style={'Alternative'} 
             onClick={() => setCreateModal(true)}
           />
           {createModal ?
             <AdminDashboardCreateModal behavior={subBehavior} id={map._id} setCreateModal={setCreateModal}/>  
           : null}
         </div>
-        <div className='admin-dashboard-content-table-entry-dropdown'>
-          <Button
-            text='Dropdown'
-            style={'Alternative'}
-            onClick={() => setDropdown((prev) => !prev)}
-          />
+        <div className='admin-dashboard-content-table-entry-dropdown pointer'>
+          <svg onClick={() => setDropdown((prev) => !prev)} height="" width="20" viewBox="0 0 20 20">
+            <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" fill="#1EDC98"></path>
+          </svg>
         </div>
         </div>
         )}
