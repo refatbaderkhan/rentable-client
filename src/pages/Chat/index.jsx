8195fd7fdfb8335a5cacb15876e9180cd8b67084
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-29 11:29:26 +0300

    added loading dependencies to Chat component and updated style

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -4,11 +4,7 @@ import { sendRequest } from '../../core/config/request';
 import { requestMethods } from '../../core/enums/requestMethods';
 import { useCustomDispatch } from '../../redux/customHooks/customDispatch';
 import { useCustomSelector } from '../../redux/customHooks/customSelector';
-import Input from "../../components/base/Input"
-import ChatCard from '../../components/ui/ChatCard';
-import Button from '../../components/base/Button';
 import { useParams } from 'react-router-dom';
-import { generateImageUrl } from '../../core/config/generateImageUrl';
 import ChatMenu from '../../components/ui/ChatMenu';
 import ChatMessages from '../../components/ui/ChatMessages';
 import ChatSend from '../../components/ui/ChatSend';
@@ -18,19 +14,25 @@ const Chat = () => {
   
   const {id} = useParams();
   const [room_id, setRoomId] = useState('');
-  const {user_id , socket} = useCustomSelector();
+  const {user_id , socket, users, chats, getUserById} = useCustomSelector();
+  const [contact, setContact] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const joinHandler = async () => {
+
+    if (!socket) return;
 
     try {
       const response = await sendRequest({
         method: requestMethods.GET,
         route: `/user/chat/${id}`,
       });
-
+      const user = getUserById(id);
+       setContact(user);
       setRoomId(response._id);
       
       socket.emit('join_chat', {user_id, room_id});
+      setLoading(false);
     } catch (error) {
       console.log(error.response.data.message);
     }
@@ -38,19 +40,28 @@ const Chat = () => {
 
   useEffect(() => {
     joinHandler();
-  }, []);
+  }, [socket, users]);
 
+  useEffect(() => {
+    setLoading(true);
+    joinHandler();
+  }, [id, chats]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (contact) {
   return (
     <div className="chat-page-container">
       <ChatMenu/>
       <div className='chat-messaging'>
         <div className='chat-messaging-header'>
           <div className='chat-messaging-name'>
-            Refat Baderkhan
+            {contact.first_name} {contact.last_name}
           </div>
         </div>
         <div className='chat-messaging-messages'>
-          <ChatMessages room_id={room_id}/>
+          <ChatMessages room_id={room_id} socket={socket}/>
         </div>
         <div>
         <ChatSend room_id={room_id}/>
@@ -58,6 +69,7 @@ const Chat = () => {
       </div>
     </div>
   )
+  }
 };
 
 export default Chat
