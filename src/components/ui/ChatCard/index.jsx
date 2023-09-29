2c403abb7dc886415c188c35b4ea8586440a3c4e
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-29 11:23:33 +0300

    updated ChatCard component functionality and style

--- src/components/ui/ChatCard/index.jsx
+++ src/components/ui/ChatCard/index.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import { useCustomSelector } from '../../../redux/customHooks/customSelector'
 import { generateImageUrl } from '../../../core/config/generateImageUrl'
+import { useNavigate } from 'react-router-dom'
+import { useCustomDispatch } from '../../../redux/customHooks/customDispatch'
 
-const ChatCard = ({chat, key}) => {
+const ChatCard = ({chat}) => {
 
-  const { user_id } = useCustomSelector()
-  const user = chat.chat_users.find(user => user.id !== user_id)
-  const last = chat.messages[chat.messages.length - 1]
-  const lastMessage = last.message
+  const navigate = useNavigate()
+  
+  const { user_id, chatId} = useCustomSelector()
+  const {setChat} = useCustomDispatch()
+  const user = chat.chat_users.find(user => user._id !== user_id)
+  const [last, setLast] = useState("")
+  const [active, setActive] = useState('chat-card')
+  const [loading, setLoading] = useState(true)
 
+  const handleChatNav = () => {
+    navigate(`/chat/${user._id}`)
+    setChat({room_id: user._id})
+  }
+
+  useEffect(() => {
+    setLast(chat.messages[chat.messages.length - 1])
+    if (chatId === user._id) {
+      setActive('chat-card-active')
+    } else {
+      setActive('chat-card')
+    }
+    setLoading(false)
+  }
+  , [chatId, chat])
+
+  if (loading) return (<div>Loading</div>)
+  if (last) {
   return (
-    <div className='chat-card-container'>
-      <div className='chat-card'>
+    <div className='chat-card-container' onClick={() => handleChatNav()}>
+      <div className={active}>
         <div className='chat-card-avatar'>
           { user.profile_picture ?
             <img src={generateImageUrl(user.profile_picture)} alt='chat-card-picture' className='chat-card-picture' />
             :
             <div className='chat-card-letter'>
-              {user.username[0]}
+              {user.username[0].toUpperCase()}
             </div>
           }
         </div>
@@ -27,12 +51,13 @@ const ChatCard = ({chat, key}) => {
             {user.username}
           </div>
           <div className='chat-card-last-message'>
-            {lastMessage}
+            {last.message}
           </div>
         </div>
       </div>
     </div>
   )
+}
 }
 
 export default ChatCard
