a7f01f9c8febbfce301620553d94dae022440998
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-29 15:06:51 +0300

    added star rating function and element to ItemCard component

--- src/components/base/ItemCard/index.jsx
+++ src/components/base/ItemCard/index.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import './style.css'
 import { useNavigate } from 'react-router-dom'
 import { generateImageUrl } from '../../../core/config/generateImageUrl'
 
 const ItemCard = ({item}) => {
 
+  const [averageRating, setAverageRating] = useState(0)
+
   const navigate = useNavigate()
+
+  const calcluateAvergeRating = () => {
+    let total = 0
+    if (item.item_ratings.length > 0) {
+      for (const rating of item.item_ratings) {
+        total += rating.rating
+      }
+      const average = total / item.item_ratings.length
+      setAverageRating(average.toFixed(1))
+    } else {
+      setAverageRating(0)
+    }
+  }
+
+  useEffect(() => {
+    calcluateAvergeRating()
+  }
+  , [item])
 
   return (
     <div key={item._id} className='card-container' onClick={()=>navigate(`/item/${item._id}`)}>
@@ -41,7 +61,7 @@ const ItemCard = ({item}) => {
             ${item.item_price}/day
           </div>
           <div className='item-rating'>
-            5.0(7)
+            <p>{averageRating}<span className='yellow-14'>&#9733;</span> </p>
           </div>
         </div>
       </div>
